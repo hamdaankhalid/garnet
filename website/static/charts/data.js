93404a4e16f6740600924fc66371009f084b51dd
github-action-benchmark <github@users.noreply.github.com>
2025-01-10 23:55:06 +0000

    add Network.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for ae13046b4d747624724f465f05c0021e3335e8dc

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736535002130,
-  "repoUrl": "https://github.com/microsoft/garnet",
+  "lastUpdate": 1736553306733,
+  "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
     "Operations.BasicOperations (ubuntu-latest  net8.0 Release)": [
@@ -45319,6 +45319,36 @@ window.BENCHMARK_DATA = {
             "value": 95.00012009342511,
             "unit": "ns",
             "range": "± 0.20380262918741018"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "42720645+hamdaankhalid@users.noreply.github.com",
+            "name": "Hamdaan Khalid",
+            "username": "hamdaankhalid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae13046b4d747624724f465f05c0021e3335e8dc",
+          "message": "Temp fix Tsavorite CI (#909)\n\nCo-authored-by: Hamdaan Khalid <hkhalid@microsoft.com>",
+          "timestamp": "2025-01-10T15:51:23-08:00",
+          "tree_id": "68a6294545c462e16ed54ce0cf4f05259e518c92",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/ae13046b4d747624724f465f05c0021e3335e8dc"
+        },
+        "date": 1736553302680,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 95.4594960468156,
+            "unit": "ns",
+            "range": "± 0.3494477173236237"
           }
         ]
       }
