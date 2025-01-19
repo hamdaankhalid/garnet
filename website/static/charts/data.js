fc339a78505d9e3c6d26d9188530e28e4ab99936
github-action-benchmark <github@users.noreply.github.com>
2025-01-19 02:30:35 +0000

    add Operations.PubSubOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for e9f6b5094a88b5285280e0ef2736d635304cbbd6

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737253821032,
+  "lastUpdate": 1737253835221,
   "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
@@ -51947,6 +51947,50 @@ window.BENCHMARK_DATA = {
             "value": 152081.25,
             "unit": "ns",
             "range": "± 18437.373085952513"
+          }
+        ]
+      }
+    ],
+    "Operations.PubSubOperations (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "96085550+vazois@users.noreply.github.com",
+            "name": "Vasileios Zois",
+            "username": "vazois"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9f6b5094a88b5285280e0ef2736d635304cbbd6",
+          "message": "fix bug with getting long value (#922)",
+          "timestamp": "2025-01-17T18:04:11-07:00",
+          "tree_id": "38828cbecbb22af2242609dfb567672c8c565ce2",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/e9f6b5094a88b5285280e0ef2736d635304cbbd6"
+        },
+        "date": 1737253832531,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 13791.831084187825,
+            "unit": "ns",
+            "range": "± 189.72256736646693"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 13831.726848895732,
+            "unit": "ns",
+            "range": "± 76.5288146376927"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 13952.282270304362,
+            "unit": "ns",
+            "range": "± 122.69467405777962"
           }
         ]
       }
