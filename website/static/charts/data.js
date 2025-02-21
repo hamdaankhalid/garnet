bd9f909036e0ca1ebe09d6b3ede1828cd2fe402f
github-action-benchmark <github@users.noreply.github.com>
2025-02-21 21:42:50 +0000

    add Network.BasicOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for b37bcc0a112c122274335c0ca49fe1482f29735f

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740174165273,
+  "lastUpdate": 1740174170485,
   "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
@@ -63099,6 +63099,36 @@ window.BENCHMARK_DATA = {
             "value": 82.45530128479004,
             "unit": "ns",
             "range": "± 0.08108405834669373"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15467143+TedHartMS@users.noreply.github.com",
+            "name": "Ted Hart",
+            "username": "TedHartMS"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b37bcc0a112c122274335c0ca49fe1482f29735f",
+          "message": "Pending iterator keycompare (#1037)\n\n* Add test and comments to confirm ContinuePendingConditionalScanPush and ContinuePendingConditionalCopyToTail behave as expected in the presence of collisions.\n\n* formatting",
+          "timestamp": "2025-02-19T18:16:54-08:00",
+          "tree_id": "5fd6e5ef648c6c6f01d9d74d2ccc931e33c2ded9",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/b37bcc0a112c122274335c0ca49fe1482f29735f"
+        },
+        "date": 1740174151090,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 84.6583916590764,
+            "unit": "ns",
+            "range": "± 0.18573705080738656"
           }
         ]
       }
