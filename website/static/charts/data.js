c55965d4ac35a44677764956dee1fe604cba06bb
github-action-benchmark <github@users.noreply.github.com>
2025-02-21 21:38:30 +0000

    add Cluster.ClusterMigrate (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for b37bcc0a112c122274335c0ca49fe1482f29735f

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740173892592,
+  "lastUpdate": 1740173910791,
   "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
@@ -3239,6 +3239,54 @@ window.BENCHMARK_DATA = {
             "value": 31881.596583439754,
             "unit": "ns",
             "range": "± 41.20881147467897"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15467143+TedHartMS@users.noreply.github.com",
+            "name": "Ted Hart",
+            "username": "TedHartMS"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b37bcc0a112c122274335c0ca49fe1482f29735f",
+          "message": "Pending iterator keycompare (#1037)\n\n* Add test and comments to confirm ContinuePendingConditionalScanPush and ContinuePendingConditionalCopyToTail behave as expected in the presence of collisions.\n\n* formatting",
+          "timestamp": "2025-02-19T18:16:54-08:00",
+          "tree_id": "5fd6e5ef648c6c6f01d9d74d2ccc931e33c2ded9",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/b37bcc0a112c122274335c0ca49fe1482f29735f"
+        },
+        "date": 1740173906788,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Get(Params: None)",
+            "value": 38704.89211164202,
+            "unit": "ns",
+            "range": "± 276.3282084444677"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Set(Params: None)",
+            "value": 38693.422022501625,
+            "unit": "ns",
+            "range": "± 45.74764815907282"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MGet(Params: None)",
+            "value": 32133.737703763523,
+            "unit": "ns",
+            "range": "± 49.65675383949242"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MSet(Params: None)",
+            "value": 31956.972722371418,
+            "unit": "ns",
+            "range": "± 32.874095367905646"
           }
         ]
       }
