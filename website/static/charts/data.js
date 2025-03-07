4caef35e484b3105b88d30fab784b4a910675a08
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 02:12:17 +0000

    add Operations.PubSubOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 6cee2afcfc2a43001b987d2c1a3859c6dec844eb

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741313534093,
+  "lastUpdate": 1741313536791,
   "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
@@ -74661,6 +74661,48 @@ window.BENCHMARK_DATA = {
             "value": 16557.203470865887,
             "unit": "ns",
             "range": "± 11.439310144711168"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "52283326+prvyk@users.noreply.github.com",
+            "name": "prvyk",
+            "username": "prvyk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6cee2afcfc2a43001b987d2c1a3859c6dec844eb",
+          "message": "Check number of keys in EVAL/EVALSHA (#1067)\n\nReturn correct type in SCRIPT EXISTS\n\nCo-authored-by: prvyk <github@privatemail.fastmailbox.net>",
+          "timestamp": "2025-03-06T17:26:54-08:00",
+          "tree_id": "c55bdf0c036e8821a457f758a1b35016e5c78da7",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/6cee2afcfc2a43001b987d2c1a3859c6dec844eb"
+        },
+        "date": 1741313523126,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 16792.5395711263,
+            "unit": "ns",
+            "range": "± 38.76400007403203"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 16742.02880859375,
+            "unit": "ns",
+            "range": "± 17.85488688597726"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 16815.49791608538,
+            "unit": "ns",
+            "range": "± 19.482710231368067"
           }
         ]
       }
