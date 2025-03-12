cab52f5b1c8956837384fb75409d65edb3ce8f3f
github-action-benchmark <github@users.noreply.github.com>
2025-03-12 20:01:54 +0000

    add Operations.PubSubOperations (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 0ea732d10bbc85435e518104e32f76754c22bd25

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741809588344,
+  "lastUpdate": 1741809713955,
   "repoUrl": "https://github.com/hamdaankhalid/garnet",
   "entries": {
     "Benchmark": [],
@@ -87099,6 +87099,68 @@ window.BENCHMARK_DATA = {
             "value": 569.0164213914138,
             "unit": "ns",
             "range": "± 0.8008199029440368"
+          }
+        ]
+      }
+    ],
+    "Operations.PubSubOperations (ubuntu-latest  net9.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "talzacc@microsoft.com",
+            "name": "Tal Zaccai",
+            "username": "TalZaccai"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0ea732d10bbc85435e518104e32f76754c22bd25",
+          "message": "Updating nanoserver Dockerfile (#1092)",
+          "timestamp": "2025-03-11T20:58:49-07:00",
+          "tree_id": "d261ab7cbe173e229555b870a0cf7595533a6189",
+          "url": "https://github.com/hamdaankhalid/garnet/commit/0ea732d10bbc85435e518104e32f76754c22bd25"
+        },
+        "date": 1741809709698,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 19873.872805786134,
+            "unit": "ns",
+            "range": "± 73.38242122482491"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 19000.99471106896,
+            "unit": "ns",
+            "range": "± 10.768015088126882"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 19733.36381312779,
+            "unit": "ns",
+            "range": "± 79.95491506174379"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 18669.631461510293,
+            "unit": "ns",
+            "range": "± 20.732538209078896"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 19091.479959341195,
+            "unit": "ns",
+            "range": "± 18.09048093639348"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 19029.576710292273,
+            "unit": "ns",
+            "range": "± 105.32757829633276"
           }
         ]
       }
